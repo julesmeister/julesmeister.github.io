b090045fe1e1ba806b65e9878e1e2184adf08e9d
Jules Lee <jackconsolidated@gmail.com>
2024-11-30 16:21:55 +0800

    Update home.jsx

--- app/routes/home/home.jsx
+++ app/routes/home/home.jsx
@@ -41,8 +41,8 @@ export const links = () => {
 
 export const meta = () => {
   return baseMeta({
-    title: 'Designer + Developer',
-    description: `Design portfolio of ${config.name} — a product designer working on web & mobile apps with a focus on motion, experience design, and accessibility.`,
+    title: 'Developer + React',
+    description: `Design portfolio of ${config.name} — a developer working on web & mobile apps.`,
   });
 };
 
